e8fb00c503ac65f3fbfc24d402cc8d47b79a9f1c
Dmitry Fuks <difuks@gmail.com>
2020-11-20 16:58:59 +0300

    fix: hard fix for static

--- src/server/AppModule.ts
+++ src/server/AppModule.ts
@@ -2,17 +2,34 @@ import { Module } from '@nestjs/common';
 import { RenderModule } from 'nest-next';
 import Next from 'next';
 import path from 'path';
+import fs from 'fs';
 
 import { IndexController } from '@server/controllers/IndexController';
 
+const next = Next({
+  dev: process.env.NODE_ENV !== 'production',
+  dir: path.resolve(__dirname, '../..'),
+});
+
+const projectPath = path.resolve(__dirname, '../..');
+
+next.serveStatic = async (req, res: any) => {
+  if (!req.url) {
+    res.send();
+
+    return;
+  }
+
+  const filePath = path.join(projectPath, req.url.replace('_next', '.next').replace(/\?ts=\d+/, ''));
+
+  const fileContent = fs.readFileSync(filePath);
+
+  res.send(fileContent);
+}
+
 @Module({
   imports: [
-    RenderModule.forRootAsync(
-      Next({
-        dev: process.env.NODE_ENV !== 'production',
-        dir: path.resolve(__dirname, '../..'),
-      }),
-    ),
+    RenderModule.forRootAsync(next),
   ],
   controllers: [IndexController],
 })
